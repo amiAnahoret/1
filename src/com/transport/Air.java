5d19fd00e0e7ed5fc0e65ef463502d3b19b7b80f
ami <ami@anahoret.com>
2015-06-16 09:26:08 +0300

    abstract

--- src/com/transport/Air.java
+++ src/com/transport/Air.java
@@ -1,6 +1,6 @@
 package com.transport;
 
-public class Air extends Transport {
+public abstract class Air extends Transport {
     public Air(int taxPerKm, int speed, double volume, int passengers) {
         super(taxPerKm, speed, volume, passengers);
 
